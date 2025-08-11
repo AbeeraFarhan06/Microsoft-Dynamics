420a468e15d25450d33317392a759ac6c17619d3
Abeera Farhan <mecard2021@gmail.com>
2025-08-11 20:47:04 +0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// Change this to your repo name
+const repoName = 'Microsoft-Dynamics';
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? `/${repoName}/` : '/',
+}));
+
